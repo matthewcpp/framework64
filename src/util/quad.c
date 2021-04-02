eb2d2444f2734fabd7126c7ca7a45e61cbd45b56
Matthew LaRocca <mlarocca.cpp@gmail.com>
2021-04-01 23:35:11 -0700

    Util: Create Untextured Quad

--- src/util/quad.c
+++ src/util/quad.c
@@ -22,7 +22,8 @@ static void vertex_set_p_tc(Vtx* vertex, short x, short y, short z, float s, flo
     vertex->n.ob[2] = z;
     vertex->n.flag = 0;
 
-    vertex_set_tc(vertex, s, t, texture);
+    if (texture)
+        vertex_set_tc(vertex, s, t, texture);
 
     vertex->n.n[0] = 0;
     vertex->n.n[1] = 0;
@@ -38,6 +39,29 @@ void textured_quad_set_tex_coords(Mesh* mesh, int frame, float s, float t) {
     vertex_set_tc(mesh->vertex_buffer + index + 1, s, 0.0f, mesh->textures);
     vertex_set_tc(mesh->vertex_buffer + index + 2, s, t, mesh->textures);
     vertex_set_tc(mesh->vertex_buffer + index + 3, 0.0f, t, mesh->textures);
+}
+
+static void create_quad_slice(Mesh* mesh, int primitive_index, short tl_x, short tl_y, short size, ImageSprite* texture) {
+    Primitive* primitive = mesh->primitives + primitive_index;
+
+    Vtx* vtx_base = mesh->vertex_buffer + primitive_index * 4;
+    vertex_set_p_tc(vtx_base + 0, tl_x, tl_y, 0, 0.0f, 0.0f, texture);
+    vertex_set_p_tc(vtx_base + 1, tl_x + size, tl_y, 0, 1.0, 0.0f, texture);
+    vertex_set_p_tc(vtx_base + 2, tl_x + size, tl_y - size, 0, 1.0, 1.0, texture);
+    vertex_set_p_tc(vtx_base + 3, tl_x, tl_y - size, 0, 0.0f, 1.0, texture);
+
+    gSPVertex(mesh->display_list + primitive_index * 3, mesh->vertex_buffer + primitive_index * 4, 4, 0);
+    gSP2Triangles(mesh->display_list + primitive_index * 3 + 1, 
+    0, 1, 2, 0, 
+    0, 2, 3, 0);
+    gSPEndDisplayList(mesh->display_list + primitive_index * 3 + 2);
+    
+    vec3_set(&primitive->bounding_box.min, tl_x, tl_y - size, 0.0f);
+    vec3_set(&primitive->bounding_box.max, tl_x + size, tl_y, 0.0f);
+    box_encapsulate_box(&mesh->info.bounding_box, &primitive->bounding_box);
+
+    primitive->vertices = primitive_index * 4;
+    primitive->display_list = primitive_index * 3;
 }
 
 
@@ -58,7 +82,6 @@ void textured_quad_create(Mesh* mesh, ImageSprite* texture) {
     mesh->info.display_list_count = mesh->info.primitive_count * 3;
     mesh->display_list = memalign(8, mesh->info.display_list_count * sizeof(Gfx));
 
-    
     short start_x, start_y, size;
 
     if (slice_count == 1) {
@@ -81,26 +104,9 @@ void textured_quad_create(Mesh* mesh, ImageSprite* texture) {
     
     for (int y = 0; y < texture->vslices; y++) {
         for (int x = 0; x < texture->hslices; x++) {
+            create_quad_slice(mesh, primitive_index, tl_x, tl_y, size, texture);
+
             Primitive* primitive = mesh->primitives + primitive_index;
-
-            Vtx* vtx_base = mesh->vertex_buffer + primitive_index * 4;
-            vertex_set_p_tc(vtx_base + 0, tl_x, tl_y, 0, 0.0f, 0.0f, texture);
-            vertex_set_p_tc(vtx_base + 1, tl_x + size, tl_y, 0, 1.0, 0.0f, texture);
-            vertex_set_p_tc(vtx_base + 2, tl_x + size, tl_y - size, 0, 1.0, 1.0, texture);
-            vertex_set_p_tc(vtx_base + 3, tl_x, tl_y - size, 0, 0.0f, 1.0, texture);
-
-            gSPVertex(mesh->display_list + primitive_index * 3, mesh->vertex_buffer + primitive_index * 4, 4, 0);
-            gSP2Triangles(mesh->display_list + primitive_index * 3 + 1, 
-            0, 1, 2, 0, 
-            0, 2, 3, 0);
-            gSPEndDisplayList(mesh->display_list + primitive_index * 3 + 2);
-            
-            vec3_set(&primitive->bounding_box.min, tl_x, tl_y - size, 0.0f);
-            vec3_set(&primitive->bounding_box.max, tl_x + size, tl_y, 0.0f);
-            box_encapsulate_box(&mesh->info.bounding_box, &primitive->bounding_box);
-
-            primitive->vertices = primitive_index * 4;
-            primitive->display_list = primitive_index * 3;
 
             primitive->material.color = MATERIAL_NO_COLOR;
             primitive->material.texture = 0;
@@ -114,4 +120,38 @@ void textured_quad_create(Mesh* mesh, ImageSprite* texture) {
         tl_y -= size;
         tl_x = start_x;
     }
+}
+
+void quad_create(Mesh* mesh, s16 size, Color* color) {
+    mesh_init(mesh);
+
+    mesh->info.color_count = 1;
+    mesh->colors = memalign(8, sizeof(Lights1) * 4);
+    memcpy(mesh->colors->a.l.col, color, sizeof(Color));
+    memcpy(mesh->colors->a.l.colc, color, sizeof(Color));
+
+    Color light_color = {125, 125, 125};
+    memcpy(mesh->colors->l[0].l.col, &light_color, sizeof(Color));
+    memcpy(mesh->colors->l[0].l.colc, &light_color, sizeof(Color));
+
+    char light_dir[3] = {40, 40, 40};
+    memcpy(mesh->colors->l[0].l.dir, &light_dir[0], 3);
+
+    mesh->info.primitive_count = 1;
+    mesh->primitives = malloc(sizeof(Primitive));
+
+    mesh->info.vertex_count = 4;
+    mesh->vertex_buffer = memalign(8, sizeof(Vtx) * 4);
+
+    mesh->info.display_list_count = 3;
+    mesh->display_list = memalign(8, sizeof(Gfx) * 3);
+
+    create_quad_slice(mesh, 0, -size / 2, size / 2, size, NULL);
+
+    Primitive* primitive = mesh->primitives;
+
+    primitive->material.color = 0;
+    primitive->material.texture = MATERIAL_NO_TEXTURE;
+    primitive->material.texture_frame = MATERIAL_NO_TEXTURE;
+    primitive->material.mode = SHADING_MODE_GOURAUD;
 }
