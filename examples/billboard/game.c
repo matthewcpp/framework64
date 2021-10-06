5aa3f44c2836554293cf015aa00e4d1e49a6e335
Matthew LaRocca <mlarocca.cpp@gmail.com>
2021-10-06 11:34:27 -0700

    assets

--- examples/billboard/game.c
+++ examples/billboard/game.c
@@ -27,7 +27,7 @@ void game_init(Game* game, fw64Engine* engine) {
     vec3_set(&game->campfire.transform.scale, 0.5f, 0.5f, 0.5f);
     entity_refresh(&game->campfire);
 
-    entity_init(&game->ground, textured_quad_create_with_params(engine, FW64_ASSET_image_RGBA16grass2MM_dl, 4.0, 4.0));
+    entity_init(&game->ground, textured_quad_create_with_params(engine, FW64_ASSET_image_grass, 4.0, 4.0));
     fw64Texture* texture = fw64_material_get_texture(fw64_mesh_get_material_for_primitive(game->ground.mesh, 0));
     fw64_texture_set_wrap_mode(texture, FW64_TEXTURE_WRAP_REPEAT, FW64_TEXTURE_WRAP_REPEAT);
     quat_from_euler(&game->ground.transform.rotation, 90.0f, 0.0f, 0.0f);
@@ -41,7 +41,7 @@ void game_init(Game* game, fw64Engine* engine) {
 
     fw64MusicBank* music_bank = fw64_music_bank_load(engine->assets, FW64_ASSET_musicbank_musicbank2);
     fw64_audio_set_music_bank(engine->audio, music_bank);
-    fw64_audio_play_music(engine->audio, 3);
+    fw64_audio_play_music(engine->audio, 2);
 }
 
 void game_update(Game* game){
@@ -57,6 +57,7 @@ void game_draw(Game* game) {
     fw64_renderer_draw_static_mesh(game->engine->renderer, &game->ground.transform, game->ground.mesh);
 
     entity_billboard(&game->moon, fw64_renderer_get_camera(game->engine->renderer));
+    entity_refresh(&game->moon);
     fw64_renderer_draw_static_mesh(game->engine->renderer, &game->moon.transform, game->moon.mesh);
 
     flame_draw(&game->flame, game->engine->renderer);
