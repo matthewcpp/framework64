81a4452280fe083fd8839383aafaa45e646bb7bb
Matthew LaRocca <mlarocca.cpp@gmail.com>
2021-11-10 08:17:07 -0800

    Add default allocator

---
+++ include/framework64/allocator.h
@@ -0,0 +1,31 @@
+#pragma once
+
+/** \file allocator.h */
+
+#include <stddef.h>
+
+typedef struct fw64Allocator fw64Allocator;
+
+typedef void*(*fw64AllocatorMallocFunc)(fw64Allocator*, size_t);
+typedef void*(*fw64AllocatorReallocFunc)(fw64Allocator*, void*, size_t);
+typedef void*(*fw64AllocatorMemalignFunc)(fw64Allocator*, size_t, size_t);
+typedef void(*fw64AllocatorFreeFunc)(fw64Allocator*, void*);
+
+struct fw64Allocator{
+    fw64AllocatorMallocFunc malloc;
+    fw64AllocatorReallocFunc realloc;
+    fw64AllocatorMemalignFunc memalign;
+    fw64AllocatorFreeFunc free;
+};
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** This function is automatically card when the library is initialized.  It does not need to be called in your application. */
+void fw64_default_allocator_init();
+fw64Allocator* fw64_default_allocator();
+
+#ifdef __cplusplus
+}
+#endif
