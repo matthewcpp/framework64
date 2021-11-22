30128eee46c9fcf382cc9a35c5e41ca4c6248ce7
Matthew LaRocca <mlarocca.cpp@gmail.com>
2021-11-22 05:36:33 -0800

    Read Animation Data

---
+++ include/framework64/animation_data.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include "framework64/allocator.h"
+#include "framework64/asset_database.h"
+#include "framework64/types.h"
+
+#include <stdint.h>
+
+typedef struct {
+    uint16_t children_index;
+    uint16_t children_count;
+} fw64AnimationJoint;
+
+typedef struct {
+    uint32_t joint_count;
+    fw64AnimationJoint* joints;
+    uint16_t* hierarchy;
+    float* inverse_bind_matrices;
+} fw64AnimationSkin;
+
+/** indices into the animation data buffer */
+typedef struct {
+    uint16_t translation;
+    uint16_t rotation;
+    uint16_t scale;
+} fw64AnimationTrack;
+
+typedef struct {
+    float total_time; // time of animation in seconds
+    uint32_t key_count; // number of keys in the buffers
+    uint32_t input_index; // offset into keyframe_data of input buffer
+    uint32_t tracks_index;  // index into the track array for the first joint's track
+} fw64Animation;
+
+
+typedef struct {
+    fw64AnimationSkin skin;
+    uint32_t animation_count;
+    fw64Animation* animations;
+    fw64AnimationTrack* tracks;
+    float* keyframe_data;
+} fw64AnimationData;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+fw64AnimationData* fw64_animation_data_load(fw64AssetDatabase* database, uint32_t index, fw64Allocator* allocator);
+void fw64_animation_data_delete(fw64AnimationData* animation_data, fw64Allocator* allocator);
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
---
+++ src/animation_data.c
@@ -0,0 +1,57 @@
+#include "framework64/animation_data.h"
+#include "framework64/filesystem.h"
+
+typedef struct {
+    uint32_t joint_count;
+    uint32_t joint_hierarchy_size;
+    uint32_t animation_count;
+    uint32_t track_data_size;
+    uint32_t keyframe_data_size;
+} fw64AnimationInfo;
+
+fw64AnimationData* fw64_animation_data_load(fw64AssetDatabase* database, uint32_t index, fw64Allocator* allocator) {
+    if (allocator == NULL) allocator = fw64_default_allocator();
+
+    int handle = fw64_filesystem_open(index);
+
+    if (handle < 0)
+        return NULL;
+
+    fw64AnimationData* data = allocator->malloc(allocator, sizeof(fw64AnimationData));
+
+    fw64AnimationInfo info;
+    fw64_filesystem_read(&info, sizeof(fw64AnimationInfo), 1, handle);
+
+    data->skin.joint_count = info.joint_count;
+    char* data_ptr = allocator->malloc(allocator, info.joint_hierarchy_size);
+    fw64_filesystem_read(data_ptr, info.joint_hierarchy_size, 1, handle);
+    data->skin.joints = (fw64AnimationJoint *)data_ptr;
+    data->skin.hierarchy = (uint16_t*)(data_ptr + info.joint_count * sizeof(fw64AnimationJoint));
+
+    size_t inv_bind_matrices_size = info.joint_count * 16 * sizeof(float);
+    data->skin.inverse_bind_matrices = allocator->malloc(allocator, inv_bind_matrices_size);
+    fw64_filesystem_read(data->skin.inverse_bind_matrices, info.joint_count, 16 * sizeof(float), handle);
+
+    data->animation_count = info.animation_count;
+    data->animations = allocator->malloc(allocator, sizeof(fw64Animation) * info.animation_count);
+    fw64_filesystem_read(data->animations, sizeof(fw64Animation), info.animation_count, handle);
+
+    data->tracks = allocator->malloc(allocator, info.track_data_size);
+    fw64_filesystem_read(data->tracks, 1, info.track_data_size, handle);
+
+    data->keyframe_data = allocator->malloc(allocator, info.keyframe_data_size);
+    fw64_filesystem_read(data->keyframe_data, 1, info.keyframe_data_size, handle);
+
+    fw64_filesystem_close(handle);
+
+    return data;
+}
+
+void fw64_animation_data_delete(fw64AnimationData* data, fw64Allocator* allocator) {
+    allocator->free(allocator, data->skin.joints); // note: joint hierarchy consists of single allocation
+    allocator->free(allocator, data->skin.inverse_bind_matrices);
+    allocator->free(allocator, data->animations);
+    allocator->free(allocator, data->tracks);
+    allocator->free(allocator, data->keyframe_data);
+    allocator->free(allocator, data);
+}
