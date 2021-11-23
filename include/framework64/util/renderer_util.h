4a834ee26593db37f6458f45c42cca5ca5e36781
Matthew LaRocca <mlarocca.cpp@gmail.com>
2021-11-22 22:07:19 -0800

    fullscreen overlay n64

---
+++ include/framework64/util/renderer_util.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "framework64/renderer.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void fw64_renderer_util_fullscreen_overlay(fw64Renderer* renderer, uint8_t r, uint8_t g, uint8_t b, uint8_t a);
+
+#ifdef __cplusplus
+}
+#endif
