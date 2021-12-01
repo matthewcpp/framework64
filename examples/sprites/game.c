4ad78fe695d48444e6565cb89c0dc5b689a000ce
Matthew LaRocca <mlarocca.cpp@gmail.com>
2021-12-01 05:24:54 -0800

    Update sprites sample

--- examples/sprites/game.c
+++ examples/sprites/game.c
@@ -23,6 +23,9 @@ void game_init(Game* game, fw64Engine* engine) {
     elapsed_time_init(&game->elapsed_time, fw64_font_load(engine->assets, FW64_ASSET_font_basicLAZER, NULL));
     fw64Font* sm64_font = fw64_font_load(engine->assets, FW64_ASSET_font_Mario64, NULL);
     typewriter_text_init(&game->typewriter_text, "super mario 64", sm64_font);
+
+    game->nintendo_seal = fw64_texture_create_from_image(fw64_image_load(engine->assets, FW64_ASSET_image_nintendo_seal, NULL), NULL);
+    game->overlay = fw64_texture_create_from_image(fw64_image_load(engine->assets, FW64_ASSET_image_overlay, NULL), NULL);
 }
 
 void game_update(Game* game){
@@ -41,5 +44,7 @@ void game_draw(Game* game){
     ken_sprite_draw(&game->ken_sprite, renderer);
     elapsed_time_draw(&game->elapsed_time, renderer);
     typewriter_text_draw(&game->typewriter_text, renderer);
+    fw64_renderer_draw_sprite(renderer, game->nintendo_seal, 275, 200);
+    fw64_renderer_draw_sprite(renderer, game->overlay, 275, 200);
     fw64_renderer_end(game->engine->renderer, FW64_RENDERER_FLAG_SWAP);
 }
