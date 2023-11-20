035075897934798460d832656b2663e26021e1e1
Matthew LaRocca <mlarocca.cpp@gmail.com>
2023-11-19 17:44:17 -0800

    Desktop: Remove GLB parser c++ class

--- include/framework64/desktop/splitter.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "framework64/desktop/mesh_data.h"
-
-#include <vector>
-
-namespace framework64 {
-    std::vector<MeshData> splitByJointIndex(MeshData const & mesh_data);
-}
