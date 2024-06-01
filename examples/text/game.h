b27cc1b1bd44a38efd74f9a14a28445822427815
Matthew LaRocca <mlarocca.cpp@gmail.com>
2024-06-01 05:40:15 -0700

    Add text Example

---
+++ examples/text/game.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <framework64/engine.h>
+#include <framework64/render_pass.h>
+#include <framework64/sprite_batch.h>
+
+#define FONT_COUNT 3
+
+typedef struct {
+    fw64Engine* engine;
+    fw64RenderPass* renderpass;
+    fw64SpriteBatch* sprite_batch;
+
+    fw64Font* fonts[FONT_COUNT];
+    int active_font_index;
+} Game;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void game_init(Game* game, fw64Engine* engine);
+void game_update(Game* game);
+void game_draw(Game* game);
+
+#ifdef __cplusplus
+}
+#endif
