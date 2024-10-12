009606790da3f7001fde7c710229451cbfda97e3
Matthew LaRocca <mlarocca.cpp@gmail.com>
2024-10-12 10:00:34 -0700

    Add material_collection example

---
+++ examples/material_collection/game.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "framework64/engine.h"
+#include "framework64/scene.h"
+
+#include "fw64_arcball_camera.h"
+#include "fw64_headlight.h"
+
+typedef struct {
+    fw64ColorRGBA8 hairColor, skinColor;
+    fw64Texture* textures[4];
+    
+} AlternatePalette;
+
+typedef struct {
+    fw64Engine* engine;
+    fw64Scene scene;
+    fw64Camera camera;
+    fw64ArcballCamera arcball;
+    fw64Headlight headlight;
+    fw64RenderPass* renderpass;
+    fw64MaterialCollection alternate_palette;
+} Game;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void game_init(Game* game, fw64Engine* engine);
+void game_update(Game* game);
+void game_draw(Game* game);
+
+#ifdef __cplusplus
+}
+#endif
