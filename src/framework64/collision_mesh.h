1ce0308ca62badc593789116f5c10cf2c76a4920
Matthew LaRocca <mlarocca.cpp@gmail.com>
2025-07-06 06:18:51 -0700

    Collision Mesh: add utility method for getting triangle count

--- src/framework64/collision_mesh.h
+++ src/framework64/collision_mesh.h
@@ -12,14 +12,29 @@
 
 typedef struct {
     uint32_t point_count;
+
+    /** This is the total number of items in elements
+     *  The total number of triangles is element_count / 3
+     */
     uint32_t element_count;
     Box box;
     Vec3* points;
     uint16_t* elements;
 } fw64CollisionMesh;
 
-int fw64_collision_mesh_test_sphere(fw64CollisionMesh* collision_mesh, fw64Transform* transform, Vec3* center, float radius, Vec3* out_point);
-int fw64_collision_mesh_test_ray(fw64CollisionMesh* collision_mesh, fw64Transform* transform, Vec3* origin, Vec3* direction, Vec3* out_point, float* out_dist);
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 
 int fw64_collision_mesh_load_from_datasource(fw64CollisionMesh* collision_mesh, fw64DataSource* data_source, fw64Allocator* allocator);
 void fw64_collision_mesh_uninit(fw64CollisionMesh* collision_mesh, fw64Allocator* allocator);
+
+int fw64_collision_mesh_test_sphere(const fw64CollisionMesh* collision_mesh, fw64Transform* transform, Vec3* center, float radius, Vec3* out_point);
+int fw64_collision_mesh_test_ray(const fw64CollisionMesh* collision_mesh, fw64Transform* transform, Vec3* origin, Vec3* direction, Vec3* out_point, float* out_dist);
+
+#define fw64_collision_mesh_get_triangle_count(collision_mesh) ((collision_mesh)->element_count / 3)
+
+#ifdef __cplusplus
+}
+#endif
