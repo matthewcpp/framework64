02544fb69fa043b615e080496fa9e70e59e23e4d
Matthew LaRocca <mlarocca.cpp@gmail.com>
2025-08-01 05:58:53 -0700

    Vec: add length_squared methods

--- src/framework64/vec2.c
+++ src/framework64/vec2.c
@@ -1,5 +1,8 @@
 #include "framework64/vec2.h"
 
+#include "framework64/types.h"
+#include "framework64/math.h"
+
 void vec2_set(Vec2* v, float x, float y) {
     v->x = x;
     v->y = y;
@@ -13,6 +16,25 @@ void vec2_add(const Vec2* a, const Vec2* b, Vec2* out) {
 void vec2_scale(const Vec2* v, float k, Vec2* out) {
     out->x = v->x * k;
     out->y = v->y * k;
+}
+
+void vec2_normalize(Vec2* v) {
+    float len = v->x * v->x + v->y * v->y;
+
+    if (len > 0) {
+        len = 1.0f / fw64_sqrtf(len);
+    }
+
+    v->x *= len;
+    v->y *= len;
+}
+
+float vec2_length(const Vec2* v) {
+    return fw64_sqrtf(v->x * v->x + v->y * v->y);
+}
+
+float vec2_length_squared(const Vec2* v) {
+    return v->x * v->x + v->y * v->y;
 }
 
 void ivec2_set(IVec2* vec, int x, int y) {
