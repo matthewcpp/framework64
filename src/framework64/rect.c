1b8408a472ab2ea48004ee4bba094ed5a9377a5c
Matthew LaRocca <mlarocca.cpp@gmail.com>
2025-08-01 05:42:56 -0700

    Remove unused struct

--- src/framework64/rect.c
+++
@@ -1,7 +0,0 @@
-#include "framework64/rect.h"
-
-int fw64_rect_contains_rect(fw64Rect* a, fw64Rect* b) {
-
-    return  fw64_rect_min_x(b) >= fw64_rect_min_x(a) && fw64_rect_max_x(b) <= fw64_rect_max_x(a) &&
-            fw64_rect_min_y(b) >= fw64_rect_min_y(a) && fw64_rect_max_x(b) <= fw64_rect_max_x(a);
-}
