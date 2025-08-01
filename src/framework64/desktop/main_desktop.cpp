a88e385a9670a54151a3279520f79db2a09ec6c7
Matthew LaRocca <mlarocca.cpp@gmail.com>
2025-08-01 05:54:55 -0700

    Engine: add fixed update method

--- src/framework64/desktop/main_desktop.cpp
+++ src/framework64/desktop/main_desktop.cpp
@@ -7,8 +7,10 @@
 #include <CLI/CLI.hpp>
 #include <SDL2/SDL.h>
 
+#include <chrono>
 #include <iostream>
 #include <string>
+#include <thread>
 
 int main(int argc, char** argv) {
     framework64::Engine engine;
@@ -35,15 +37,20 @@ int main(int argc, char** argv) {
 
     SDL_Event event;
     int keep_going = 1;
-    Uint32 last_update, now, time_delta;
+
+    const uint64_t target_frame_time_ms = 1000 / settings.target_frame_rate;
+    const uint64_t fixed_timestep_ms = 1000 / settings.fixed_update_rate;
+    uint64_t accumulator = 0;
+
+    auto last_update = std::chrono::steady_clock::now();
 
     // first call to poll event can take some time as systems are initialized.
     // prime it here before tight update loop
     SDL_PollEvent(&event);
 
-    last_update = SDL_GetTicks();
-
     while (keep_going) {
+        auto frame_start = std::chrono::steady_clock::now();
+
         while(SDL_PollEvent(&event)) {
             switch (event.type) {
                 case SDL_QUIT:
@@ -60,21 +67,32 @@ int main(int argc, char** argv) {
             }
         }
 
-        now = SDL_GetTicks();
-        time_delta = now - last_update;
+        auto frame_delta_ms = std::chrono::duration_cast<std::chrono::milliseconds>(frame_start - last_update).count();
+        accumulator += frame_delta_ms;
 
-        if (time_delta >= 32) {
-            engine.update(static_cast<float>(time_delta) / 1000.0f);
-            game_update(&game);
-
-            engine.renderer->beginFrame();
-            game_draw(&game);
-            engine.renderer->endFrame();
-
-            last_update = now;
+        // do we need an engine fixed update? e.g. physics simulation
+        while (accumulator > fixed_timestep_ms) {
+            game_fixed_update(&game);
+            accumulator -= fixed_timestep_ms;
         }
 
-        SDL_Delay(1);
+        float accumulator_progress = static_cast<float>(accumulator) / static_cast<float>(fixed_timestep_ms);
+        engine.update(static_cast<float>(frame_delta_ms) / 1000.0f, accumulator_progress);
+        game_update(&game);
+
+        // todo: time to next step?
+        engine.renderer->beginFrame();
+        game_draw(&game);
+        engine.renderer->endFrame();
+
+
+        auto frame_end = std::chrono::steady_clock::now();
+        uint64_t frame_time_ms = std::chrono::duration_cast<std::chrono::milliseconds>(frame_end - frame_start).count();
+        last_update = frame_start;
+
+        if (frame_time_ms < target_frame_time_ms) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(target_frame_time_ms - frame_time_ms));
+        }
     }
 
     return 0;
--- src/framework64/desktop/settings.hpp
+++ src/framework64/desktop/settings.hpp
@@ -10,6 +10,8 @@ struct Settings {
     int screen_height = 240;
     int data_link_port = 55662;
     float display_scale = 3.0f;
+    uint32_t fixed_update_rate = 30;
+    uint32_t target_frame_rate = 60;
     std::string application_name = "framework64";
     std::string media_dir_name = "media";
     fw64SaveFile::SaveFileType save_file_type = fw64SaveFile::SaveFileType::N64Eeprom4k;
