86695e0f683aded932aa261969a83cdc5de57beb
Matthew LaRocca <mlarocca.cpp@gmail.com>
2026-06-22 16:58:19 -0700

    Desktop: when parsing material bundle, set runtime values for texture wrapping params

--- pipeline/gltf/Texture.js
+++ pipeline/gltf/Texture.js
@@ -1,15 +1,20 @@
 class Texture {
+    /** These values need to be kept in sync with desktop/material_bundle.cpp */
     static WrapMode = {
         Repeat: 0,
         Mirror: 1,
         Clamp: 2
     };
 
-    // index into GLTF data `images` array
+    /** index into GLTF data `images` array */
     image;
     wrapS = Texture.WrapMode.Repeat;
     wrapT = Texture.WrapMode.Repeat;
+
+    /** N64 Specific */
     maskS = 0;
+
+    /** N64 Specific */
     maskT = 0;
     paletteIndex = 0;
 
