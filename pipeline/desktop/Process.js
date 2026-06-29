f9859a5b0c9f3626235fef4af63afd7bf5f9871b
Matthew LaRocca <mlarocca.cpp@gmail.com>
2026-06-29 14:14:00 -0700

    desktop pipeline: remove redundant file

--- pipeline/desktop/Process.js
+++ pipeline/desktop/Process.js
@@ -1,8 +1,17 @@
+const BasicFileProcessor = require("../FileProcessor");
 const DesktopAssetBundle = require("./AssetBundle");
+const DesktopFontProcessor = require("./FontProcessor");
+const DesktopImageProcessor = require("./ImageProcessor");
+const DesktopSoundBankProcessor = require("./SoundBankProcessor");
+const DesktopMaterialBundleWriter = require("./MaterialBundleWriter");
+const DesktopMeshWriter = require("./MeshWriter");
+const DesktopMusicBankProcessor = require("./MusicBankProcessor");
 const Environment = require("../Environment");
+const GltfLevelProcessor = require("../LevelProcessor");
+const GltfMeshProcessor = require("../MeshProcessor");
+const GltfSkinnedMeshProcessor = require("../SkinnedMeshProcessor");
+const PipelineProcessor = require("../common/PipelineProcessor");
 const Util = require("../Util");
-
-const DesktopPipelineProcessor = require("./DesktopPipelineProcessor");
 
 const fs = require("fs")
 const path = require("path");
@@ -28,6 +37,26 @@ async function runPipelineProcessor(manifestFile, assetDirectory, outputDirector
     bundle.writeAssetBundle(path.join(outputDirectory, "asset_bundle.txt"));
     bundle.writeManifest(path.join(outputDirectory, "manifest.txt"))
 }
+
+class DesktopPipelineProcessor extends PipelineProcessor {
+    constructor(environment, plugins) {
+        super(environment);
+
+        this._fileProcessor = new BasicFileProcessor(environment, plugins);
+        this._musicBankProcessor = new DesktopMusicBankProcessor(environment);
+        this._soundBankProcessor = new DesktopSoundBankProcessor(environment);
+
+        this._imageProcessor = new DesktopImageProcessor(environment);
+        this._fontProcessor = new DesktopFontProcessor(environment, this._imageProcessor);
+
+        const materialBundleWriter = new DesktopMaterialBundleWriter(environment, this._imageProcessor);
+        const meshWriter = new DesktopMeshWriter(materialBundleWriter);
+    
+        this._levelProcessor = new GltfLevelProcessor(environment, materialBundleWriter, meshWriter);
+        this._meshProcessor = new GltfMeshProcessor(environment, meshWriter);
+        this._skinnedMeshProcessor = new GltfSkinnedMeshProcessor(environment, meshWriter);
+    }
+};
 
 module.exports = {
     processDesktop: processDesktop,
